1cbdaaef2dad84dab656f1adcab598eda35a5fb2
Martin Nilsson <ibbles@hotmail.com>
2017-09-17 15:06:06 +0200

    On error, move cursor to error location

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
+import re
 import sys
 import subprocess
 import tempfile
@@ -20,22 +21,45 @@ form.addRow(input)
 run = QPushButton("&Run")
 
 def run_octave(string):
-    file = tempfile.NamedTemporaryFile(mode='w')
-    file.write(string)
-    file.flush()
-    file_name = file.name
-    result = subprocess.run(
-        ["octave", "-q", file_name],
-        stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    file.close()
-    return result
+    # Using a temporary file instead of the --eval Octave argument because
+    # Octave get line numbering wrong when not reading from a file.
+    with tempfile.NamedTemporaryFile(mode='w') as file:
+        file.write(string)
+        file.flush()
+        file_name = file.name
+        status = subprocess.run(
+            ["octave", "--no-gui", "-q", file_name],
+            stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        return status
+
+def move_to_error(error_string):
+        line_col = re.match(".*near line (.*) column (.*)", error_string)
+        if line_col:
+            try:
+                # -1 becase cursor movements are 0-indexed but Octave line/col
+                # numbering is not.
+                line = int(line_col.group(1)) - 1
+                col = int(line_col.group(2)) - 1
+                cursor = input.textCursor()
+                cursor.setPosition(0)
+                cursor.movePosition(QTextCursor.Down, n = line - 1)
+                cursor.movePosition(QTextCursor.Right, n = col - 1)
+                input.setTextCursor(cursor)
+            except:
+                # Could not parse error message. That's fine, just cannot
+                # helfully place the cursor where the error is.
+                pass
 
 def on_clicked():
-    result = run_octave(input.toPlainText())
-    if result.returncode == 0:
-        output.setText(result.stdout.decode("utf-8"))
+    script = input.toPlainText()
+    status = run_octave(script)
+    if status.returncode == 0:
+        result = status.stdout.decode("utf-8")
+        output.setText(result)
     else:
-        output.setText(result.stderr.decode("utf-8"))
+        error = status.stderr.decode("utf-8")
+        output.setText(error)
+        move_to_error(error)
 
 run.clicked.connect(on_clicked)
 form.addRow(run)
