8cecd69c9b036772642dd886269a7f296eb64b42
Martin Nilsson <ibbles@hotmail.com>
2017-09-20 19:14:55 +0200

    Fix typo in introduction

--- main.py
+++ main.py
@@ -3,11 +3,11 @@
 #
 
 
-# InteractiveCalculator is an Octave script editor that displays the Octave
-# output as the user types. The intended use case is for quick throw-away
-# exploratory computations where the problem isn't well enough understood for
-# the Octave shell and creating files and re-running Octave by hand several
-# times is too much overhead.
+# IterativeCalculator is an Octave script editor that displays the Octave output
+# as the user types. The intended use case is for quick throw-away exploratory
+# computations where the problem isn't well enough understood for the Octave
+# shell and creating files and re-running Octave by hand several times is too
+# much overhead.
 
 
 import os
