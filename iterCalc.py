d241c562dcd850aabe4da07b5d704c34487cd19b
Martin Nilsson <ibbles@hotmail.com>
2017-09-20 19:18:35 +0200

    Fix grammar in introduction

--- iterCalc.py
+++ iterCalc.py
@@ -5,7 +5,7 @@
 
 # IterativeCalculator is an Octave script editor that displays the Octave output
 # as the user types. The intended use case is for quick throw-away exploratory
-# computations where the problem isn't well enough understood for the Octave
+# computations where the problem isn't understood well enough for the Octave
 # shell and creating files and re-running Octave by hand several times is too
 # much overhead.
 
